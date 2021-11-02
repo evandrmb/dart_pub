c7131f04670aa9ca94e99d29656c0fb640c5799c
evandrojr <47798920+evandrmb@users.noreply.github.com>
2021-11-01 22:03:31 -0300

    hard coded database uri

--- bin/server.dart
+++ bin/server.dart
@@ -2,7 +2,8 @@ import 'package:unpub/unpub.dart' as unpub;
 
 void main(List<String> args) async {
   const basedir = '/packages';
-  const db = String.fromEnvironment('MONGODB_URI');
+  const db =
+      'mongodb://ap_master:L043Ya1aMiWiaCnFijUF@apogeupubdev-shard-00-00.93vch.mongodb.net:27017,apogeupubdev-shard-00-01.93vch.mongodb.net:27017,apogeupubdev-shard-00-02.93vch.mongodb.net:27017/ApogeuPubDev?ssl=true&replicaSet=atlas-kmsagm-shard-0&authSource=admin&retryWrites=true&w=majority';
 
   final metaStore = unpub.MongoStore(db);
   await metaStore.db.open();
