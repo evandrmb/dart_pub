b04f64fea5a32d7ff4ee7259fd36a023412fa54d
evandrojr <47798920+evandrmb@users.noreply.github.com>
2021-11-01 21:59:49 -0300

    added Atlas MongoDB instance

--- bin/server.dart
+++ bin/server.dart
@@ -1,21 +1,19 @@
 import 'package:unpub/unpub.dart' as unpub;
 
 void main(List<String> args) async {
-  print(args);
+  const basedir = '/packages';
+  const db = String.fromEnvironment('MONGODB_URI');
 
-  var basedir = '/packages'; // Base directory to save pacakges
-  var db = 'mongodb://mongo:27017/dart_pub'; // MongoDB uri
-
-  var metaStore = unpub.MongoStore(db);
+  final metaStore = unpub.MongoStore(db);
   await metaStore.db.open();
 
-  var packageStore = unpub.FileStore(basedir);
+  final packageStore = unpub.FileStore(basedir);
 
-  var app = unpub.App(
+  final app = unpub.App(
     metaStore: metaStore,
     packageStore: packageStore,
   );
 
-  var server = await app.serve('0.0.0.0', 8080);
+  final server = await app.serve('0.0.0.0', 8080);
   print('Serving at http://${server.address.host}:${server.port}');
 }
